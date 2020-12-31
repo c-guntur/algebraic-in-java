1e5dfb0b85c4c03ebe81aefdb1148a1cb780a691
Chandra Guntur <chandra.guntur@gmail.com>
2020-12-31 18:51:33 -0500

    Added a Class and Tuple example

--- src/none/cgutils/algebraic/product/Example1Class.java
+++ src/none/cgutils/algebraic/product/Example1Class.java
@@ -2,11 +2,26 @@ package none.cgutils.algebraic.product;
 
 public class Example1Class {
 
+    public static void main(String[] args) {
+        Pojo pojo = new Pojo("Hello World", 1, Boolean.TRUE);
+        System.out.println("\n1. Pojo --->\n\t" + pojo);
+    }
+
+}
+
+class Pojo {
+
     private String string;
 
     private Integer integer;
 
     private Boolean flag;
+
+    public Pojo(String string, Integer integer, Boolean flag) {
+        this.string = string;
+        this.integer = integer;
+        this.flag = flag;
+    }
 
     public String getString() {
         return string;
@@ -31,4 +46,14 @@ public class Example1Class {
     public void setFlag(Boolean flag) {
         this.flag = flag;
     }
+
+    @Override
+    public String toString() {
+        return "Pojo{" +
+                "string='" + string + '\'' +
+                ", integer=" + integer +
+                ", flag=" + flag +
+                '}';
+    }
+
 }
---
+++ src/none/cgutils/algebraic/product/Example2Tuple.java
@@ -0,0 +1,86 @@
+package none.cgutils.algebraic.product;
+
+public class Example2Tuple {
+
+    public static void main(String[] args) {
+        Pair<Integer, String> integerStringPair = new Pair<>(1, "One");
+        System.out.println("\n1. integerStringPair --->\n\t" + integerStringPair);
+
+        Pair<String, Boolean> stringBooleanPair = new Pair<>("TRUE", Boolean.TRUE);
+        System.out.println("\n2. stringBooleanPair --->\n\t" + stringBooleanPair);
+
+        Twin<String> stringTwin = new Twin<>("One", "Uno");
+        System.out.println("\n3. stringTwin --->\n\t" + stringTwin);
+    }
+}
+
+// A PAIR IS A TWO-TUPLE OF TWO TYPES
+class Pair<T, U> {
+
+    T first;
+    U second;
+
+    public Pair(T first, U second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public void setFirst(T first) {
+        this.first = first;
+    }
+
+    public U getSecond() {
+        return second;
+    }
+
+    public void setSecond(U second) {
+        this.second = second;
+    }
+
+    @Override
+    public String toString() {
+        return "Pair{" +
+                "first=" + first.toString() +
+                ", second=" + second.toString() +
+                '}';
+    }
+}
+
+// A TWIN IS A TWO-TUPLE OF SINGLE TYPE
+class Twin<T> {
+    T first;
+    T second;
+
+    public Twin(T first, T second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public void setFirst(T first) {
+        this.first = first;
+    }
+
+    public T getSecond() {
+        return second;
+    }
+
+    public void setSecond(T second) {
+        this.second = second;
+    }
+
+    @Override
+    public String toString() {
+        return "Twin{" +
+                "first=" + first +
+                ", second=" + second +
+                '}';
+    }
+}
